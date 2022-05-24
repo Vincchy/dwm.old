677b78b31e3bd7d51075eca6be2bf9364426f7dc
vi <vi@ncurses.localdomain>
2022-05-24 17:10:48 +0200

    Added dwm-pertag patch

--- drw.c
+++ drw.c
@@ -140,16 +140,11 @@ xfont_create(Drw *drw, const char *fontname, FcPattern *fontpattern)
 	 * https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=916349
 	 * and lots more all over the internet.
 	 */
-
-
-	// I have no idea what it does Bugswriter told me to comment this
-	/*
 	FcBool iscol;
 	if(FcPatternGetBool(xfont->pattern, FC_COLOR, 0, &iscol) == FcResultMatch && iscol) {
 		XftFontClose(drw->dpy, xfont);
 		return NULL;
 	}
-	*/
 
 	font = ecalloc(1, sizeof(Fnt));
 	font->xfont = xfont;
